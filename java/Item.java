a5cba4a6cac482aa1448386d87c942dbbfa3ad18
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-07-13 18:47:49 +0900

    commit

---
+++ java/Item.java
@@ -0,0 +1,38 @@
+public class Item {
+  private int num = 10;
+  public void setNum(int num) {
+    this.num = num;
+  }
+  public int getNum() {
+    return this.num;
+  }
+}
+
+
+// package calcapp.logics;
+// public class CalcLogic {
+//   public static int tasu(int a, int b) {
+//     return (a + b);
+//   }
+//   public static int hiku(int a, int b) {
+//     return (a - b);
+//   }
+// }
+
+
+// package ex6;
+// public class Sample {
+//   public static int num = 0;
+//   public static void print() {
+//     System.out.println(num);
+//   }
+// }
+
+
+
+
+
+// public class Sub {
+//   String content = "Subクラスのフィールド値です。";
+ 
+// }
---
+++ java/Main.java
@@ -0,0 +1,72 @@
+
+aaa
+
+
+
+
+// public class Main {
+//   public static void main(String[] args) {
+//     int a = 10;
+//     int b = a++ + a + a-- - a-- + ++a;
+//     System.out.println(b);
+//   }
+// }
+
+
+
+// public class Main {
+//   public static void main(String[] args) {
+//     Item a = new Item();
+//     Item b = new Item();
+//     b.setNum(20);
+//     System.out.println(a.getNum());
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// package calcapp.main;
+// public class Calc {
+//     public static void main (String[] args) {
+//         int a = 10;
+//         int b = 2;
+//         int total = calcapp.logics.CalcLogic.tasu(a, b);
+//         int delta = calcapp.logics.CalcLogic.hiku(a, b);
+//         System.out.println("足すと" + total + "、引くと" + delta);
+//     }  
+// }
+
+
+
+
+
+
+
+
+// import static ex6.Sample.num;
+// import static ex6.Sample.print;
+
+
+// public class Main {
+//   public static void main(String[] args) {
+//     num = 10;
+//     print();
+//   }
+// }
+
+
+// public class Main {
+// public static void main(String args[]) {
+//     Sub sub = new Sub();
+//     System.out.println(sub.content);
+//   }
+// }
