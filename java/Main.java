4c1754c65cdeace5e57b96c00a20b58dd5d34791
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-07-15 00:19:24 +0900

    commit

--- java/Main.java
+++ java/Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
   public static void main(String[] args) {
-    int array[][] = new int[][] {{1,2},{2,3,4}};
+    int array[][] = new int[][] {{1,2},{3,4}};
     int total = 0;
     for (int i = 0; i < array.length; i++) {
       for (int j = i; j < array[i].length; j++) {
